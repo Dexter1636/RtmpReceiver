4f254bedfd93ecee8ff68922311998298f040874
Dexter <2779858398@qq.com>
2018-11-24 21:43:56 +0800

    core done

--- app/src/main/java/org/blackant/rtmpreceiver/ui/VideoActivity.java
+++ app/src/main/java/org/blackant/rtmpreceiver/ui/VideoActivity.java
@@ -1,11 +1,16 @@
 package org.blackant.rtmpreceiver.ui;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 import com.ksyun.media.player.IMediaPlayer;
@@ -14,29 +19,92 @@ import com.ksyun.media.player.KSYMediaPlayer;
 import org.blackant.rtmpreceiver.R;
 import org.blackant.rtmpreceiver.qyplayerutils.QosThread;
 
+import java.io.IOException;
 
-public class VideoActivity extends AppCompatActivity {
 
+public class VideoActivity extends Activity {
+
+    // 视频配置参数
     private int mVideoWidth = 0;
     private int mVideoHeight = 0;
     private int mVideoScaleIndex = 0;
+    private boolean useHwCodec = true;
+    String mVideoUrl = "rtmp://live.hkstv.hk.lxdns.com/live/hks1";
 
+    // 播放器的对象
     private KSYMediaPlayer ksyMediaPlayer;
+    // 播放SDK提供的监听器
+    // 播放器在准备完成，可以开播时会发出onPrepared回调
+    // private IMediaPlayer.OnPreparedListener mOnPreparedListener;
+    // 播放完成时会发出onCompletion回调
+    // private IMediaPlayer.OnCompletionListener mOnCompletionListener;
+    // 播放器遇到错误时会发出onError回调
+    private IMediaPlayer.OnErrorListener mOnErrorListener;
+    private IMediaPlayer.OnInfoListener mOnInfoListener;
+    private IMediaPlayer.OnVideoSizeChangedListener mOnVideoSizeChangeListener;
+    private IMediaPlayer.OnSeekCompleteListener mOnSeekCompletedListener;
+    // SurfaceView需在Layout中定义，此处不在赘述
+    private SurfaceView mVideoSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
 
     private QosThread mQosThread;
 
     public static final int UPDATE_SEEKBAR = 0;
     public static final int HIDDEN_SEEKBAR = 1;
     public static final int UPDATE_QOS  = 2;
+
     private static final int RECORD_REQUEST_CODE  = 101;
     private static final int STORAGE_REQUEST_CODE = 102;
     private static final int AUDIO_REQUEST_CODE   = 103;
 
 
-    private View.OnClickListener mOnBtnClickListener = new View.OnClickListener() {
+    // 播放器在准备完成，可以开播时会发出onPrepared回调
+    private IMediaPlayer.OnPreparedListener mOnPreparedListener = new IMediaPlayer.OnPreparedListener() {
         @Override
-        public void onClick(View v) {
+        public void onPrepared(IMediaPlayer mp) {
+                mVideoWidth = ksyMediaPlayer.getVideoWidth();
+                mVideoHeight = ksyMediaPlayer.getVideoHeight();
+                // Set Video Scaling Mode
+                ksyMediaPlayer.setVideoScalingMode(KSYMediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+                //start player
+                ksyMediaPlayer.start();
+//                //set progress
+//                setVideoProgress(0);
+        }
+    };
 
+    // 播放完成时会发出onCompletion回调
+    private IMediaPlayer.OnCompletionListener mOnCompletionListener = new IMediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(IMediaPlayer mp) {
+            // 播放完成，用户可选择释放播放器
+            if(ksyMediaPlayer != null) {
+                ksyMediaPlayer.stop();
+                ksyMediaPlayer.release();
+            }
+        }
+    };
+
+    private final SurfaceHolder.Callback mSurfaceCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            if(ksyMediaPlayer != null) {
+                ksyMediaPlayer.setDisplay(holder);
+                ksyMediaPlayer.setScreenOnWhilePlaying(true);
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // 此处非常重要，必须调用!!!
+            if(ksyMediaPlayer != null) {
+                ksyMediaPlayer.setDisplay(null);
+            }
         }
     };
 
@@ -54,23 +122,8 @@ public class VideoActivity extends AppCompatActivity {
         }
     };
 
-    private IMediaPlayer.OnPreparedListener mOnPreparedListener = new IMediaPlayer.OnPreparedListener() {
-        @Override
-        public void onPrepared(IMediaPlayer mp) {
+    private View.OnClickListener mOnBtnClickListener = v -> {
 
-            mVideoWidth = ksyMediaPlayer.getVideoWidth();
-            mVideoHeight = ksyMediaPlayer.getVideoHeight();
-
-            // Set Video Scaling Mode
-            ksyMediaPlayer.setVideoScalingMode(KSYMediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-
-            //start player
-            ksyMediaPlayer.start();
-
-            //set progress
-            setVideoProgress(0);
-
-        }
     };
 
 
@@ -79,31 +132,58 @@ public class VideoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
-        /**
-         * 权限检查
-         */
+        mVideoSurfaceView = findViewById(R.id.surfaceView);
+
+         // 屏幕相关
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+         // 权限检查
         if (ContextCompat.checkSelfPermission(VideoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_REQUEST_CODE);
         }
-
         if (ContextCompat.checkSelfPermission(VideoActivity.this, Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[] {Manifest.permission.RECORD_AUDIO}, AUDIO_REQUEST_CODE);
         }
+
+        ksyMediaPlayer = new KSYMediaPlayer.Builder(this.getApplicationContext()).build();
+
+        ksyMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+        ksyMediaPlayer.setOnPreparedListener(mOnPreparedListener);
+        ksyMediaPlayer.setOnInfoListener(mOnInfoListener);
+        ksyMediaPlayer.setOnVideoSizeChangedListener(mOnVideoSizeChangeListener);
+        ksyMediaPlayer.setOnErrorListener(mOnErrorListener);
+        ksyMediaPlayer.setOnSeekCompleteListener(mOnSeekCompletedListener);
+
+        try {
+            ksyMediaPlayer.setDataSource(mVideoUrl);
+            ksyMediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mSurfaceHolder = mVideoSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(mSurfaceCallback);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            videoPlayEnd();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 
     public int setVideoProgress(int currentProgress) {
-
-        if(ksyMediaPlayer == null)
+        if(ksyMediaPlayer == null) {
             return -1;
-
+        }
         long time = currentProgress > 0 ? currentProgress : ksyMediaPlayer.getCurrentPosition();
         long length = ksyMediaPlayer.getDuration();
-
         return (int)time;
     }
 
@@ -113,12 +193,10 @@ public class VideoActivity extends AppCompatActivity {
             ksyMediaPlayer.release();
             ksyMediaPlayer = null;
         }
-
         if(mQosThread != null) {
             mQosThread.stopThread();
             mQosThread = null;
         }
-
         finish();
     }
 
